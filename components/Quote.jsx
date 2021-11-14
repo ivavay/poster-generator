91684605d55e0cf5f162114e70d8787e6db1cd34
Ivy Chen <36115038+ivavay@users.noreply.github.com>
2021-11-14 18:17:12 +0000

    ✨quote component w/ form created

---
+++ components/Quote.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Quote () {
+  return( 
+    <form>
+    <input type="text" />
+    <button>Get New Image</button>
+    </form>
+  )
+}
+
+export default Quote
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from "../components/Header"
+import Quote from "../components/Quote"
 import './style.css';
 
 function App() {
   return (
+    <div>
     <Header />
+    <Quote />
+    </div>
   );
 }
 
