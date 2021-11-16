a75ac038b826fcde7e56747c975b4ee9ab0dde20
Ivy Chen <36115038+ivavay@users.noreply.github.com>
2021-11-16 03:02:32 +0000

    🥳done for now!

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,22 +1,40 @@
 import React from "react"
 
 function Image () {
+  const [image, setImage] = React.useState({
+        quote: "",
+    })
+
   const [backgroundImage, setBackgroundImage] = React.useState("")
   
+  // gets the image from unsplash upon click 
   function getImage() {
     const image = `https://source.unsplash.com/collection/583204/random=${Math.random()}`
     setBackgroundImage(image)
     console.log("click") 
   }
+  // displays the typed quote 
+  function handleChange(event) {
+        const {name, value} = event.target
+        setImage(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+  }
 
   return( 
     <div>
     <div className="form">
-    <input type="text" />
+    <input  type="text" 
+            name="quote"
+            value={image.quote}
+            onChange={handleChange}
+            />
     <button onClick={getImage}>Get New Image</button>
     </div>
       <div className="image-container">
       <img src={backgroundImage} onLoad={(event) => event.target.style.display = 'inline-block'}/>
+      <h2 className="image-quote">{image.quote}</h2>
       </div>
     </div>
   )
